56e4c23791cf0ff8be42dbb056568f5145547328
Aapo Saaristo <aapo.saaristo@gmail.com>
2019-03-17 18:26:03 +0200

    initial commit

---
+++ keyboards/preonic/keymaps/shinmai/keymap.c
@@ -0,0 +1,366 @@
+/*
+TODO:
+ XXX obackspace - ctrlbSP
+ XXX numpad
+ YYY shifts
+ XXX ö '
+ XXX ä enter
+ XXX ctrl LeftBottom, super, alt, gui
+gui menu
+ XXX arrows PgUpDn endHome
+
+Figure out:
+numberrow?
+? <>| tilde +
+ XXX deL > +
+
+Bugs:
+bspace repeat
+reverse umlaut and enter?
+*/
+#include QMK_KEYBOARD_H
+#include "muse.h"
+
+enum preonic_layers {
+  _QWERTY,
+  _LOWER,
+  _RAISE,
+  _ADJUST,
+  _HYPER,
+};
+
+enum preonic_keycodes {
+  QWERTY = SAFE_RANGE,
+  MUS1, MUS2, MUS3, MUS4,
+};
+
+float m1[][2] = SONG(PLOVER_GOODBYE_SOUND);
+float m2[][2] = SONG(SONIC_RING);
+float m3[][2] = SONG(COIN_SOUND);
+float m4[][2] = SONG(MARIO_MUSHROOM);
+
+#define LSPRSE LT(_RAISE, KC_SPC)
+#define RSPLWR LT(_LOWER, KC_SPC)
+#define HYPER MO(_HYPER)
+
+typedef struct {
+  bool is_press_action;
+  int state;
+} tap;
+
+enum {
+  SINGLE_TAP = 1,
+  SINGLE_HOLD = 2,
+  DOUBLE_TAP = 3,
+  DOUBLE_HOLD = 4,
+  DOUBLE_SINGLE_TAP = 5,
+  TRIPLE_TAP = 6,
+  TRIPLE_HOLD = 7
+};
+
+enum {
+  LSCD = 0,
+  RSCD,
+  GRCL,
+  BSPTD,
+  DELPL,
+  AUMENT,
+  OUMAST,
+};
+
+int cur_dance (qk_tap_dance_state_t *state);
+
+void ls_finished (qk_tap_dance_state_t *state, void *user_data);
+void ls_reset (qk_tap_dance_state_t *state, void *user_data);
+void rs_finished (qk_tap_dance_state_t *state, void *user_data);
+void rs_reset (qk_tap_dance_state_t *state, void *user_data);
+void bs_taphandler (qk_tap_dance_state_t *state, void *user_data);
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Qwerty
+ * ,-----------------------------------------------------------------------------------.
+ * |   §  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Del+ |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  | '  Ö |EnterÄ|
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |scadet|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   -  |scadet|
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Ctrl | Hyper| Alt  | GUI  |SLower|    Space    |SRaise| Left | Down |  Up  |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_QWERTY] = LAYOUT_preonic_grid( \
+TD(GRCL),  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0,       TD(BSPTD),  \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,       TD(DELPL),  \
+  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,     KC_K,    KC_L,    TD(OUMAST), TD(AUMENT), \
+TD(LSCD),  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH,    TD(RSCD),   \
+ KC_LCTL,  HYPER,   KC_LALT, KC_LGUI, LSPRSE,  KC_SPC,  KC_SPC,  RSPLWR,   KC_LEFT, KC_DOWN, KC_UP,      KC_RGHT     \
+),
+
+/* Lower
+ * ,-----------------------------------------------------------------------------------.
+ * |   ½  |   !  |   "  |   #  |   ¤  |   %  |   &  |   /  |   (  |   )  |   =  |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |   ½  |   !  |   "  |   #  |   ¤  |   %  |   &  |   /  |   (  |   )  |   =  |  +   |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   ?  |   `  |   Å  |   ^  |  *   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO * |ISO > |  home|  end |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_LOWER] = LAYOUT_preonic_grid( \
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
+  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_END, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END \
+),
+
+/* Raise
+ * ,-----------------------------------------------------------------------------------.
+ * |   §  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |   §  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   +  |   ´  |   å  |   ¨  |  '   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ' |ISO < | pgUP | pgdw |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_RAISE] = LAYOUT_preonic_grid( \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  \
+  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END  \
+),
+
+/* Adjust (Lower + Raise)
+ * ,-----------------------------------------------------------------------------------.
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |   7  |   8  |  9   |  *   |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      | Reset|      |      |      |      |      |   4  |   5  |  6   |  -   |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |Aud on|AudOff|      |      |   1  |   2  |  3   |  +   |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |Voice-|Voice+|Mus on|MusOff|MidiOn|MidOff|   0  |   ,  |  /   |Enter |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |  m1  |  m2  |  m3  |  m4  |      |             |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_ADJUST] = LAYOUT_preonic_grid( \
+  RESET,   _______, _______, _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PAST, _______, \
+  _______, _______, _______, _______, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PMNS, _______, \
+  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PPLS, _______, \
+  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  KC_P0,   KC_PDOT, KC_PSLS, KC_PENT, _______, \
+  MUS1,    MUS2,    MUS3,    MUS4,    _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
+),
+
+/* Hyper
+ * ,-----------------------------------------------------------------------------------.
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |   7  |   8  |  9   |  *   |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      | Reset|      |      |      |      |      |   4  |   5  |  6   |  -   |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |Aud on|AudOff|      |      |   1  |   2  |  3   |  +   |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |Voice-|Voice+|Mus on|MusOff|MidiOn|MidOff|   0  |   ,  |  /   |Enter |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |  m1  |  m2  |  m3  |  m4  |      |             |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_HYPER] = LAYOUT_preonic_grid( \
+  MUS4,    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+)
+
+
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+        case QWERTY:
+          if (record->event.pressed) {
+            set_single_persistent_default_layer(_QWERTY);
+          }
+          return false;
+          break;
+        case RSPLWR:
+          if (record->event.pressed) {
+            layer_on(_LOWER);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+          } else {
+            layer_off(_LOWER);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+          }
+          return true;
+          break;
+        case LSPRSE:
+          if (record->event.pressed) {
+            layer_on(_RAISE);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+          } else {
+            layer_off(_RAISE);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+          }
+          return true;
+          break;
+        case MUS1:
+          PLAY_SONG(m1);
+          return false;
+          break;
+        case MUS2:
+          PLAY_SONG(m2);
+          return false;
+          break;
+        case MUS3:
+          PLAY_SONG(m3);
+          return false;
+          break;
+        case MUS4:
+          PLAY_SONG(m4);
+          return false;
+          break;
+      }
+    return true;
+};
+
+bool muse_mode = false;
+uint8_t last_muse_note = 0;
+uint16_t muse_counter = 0;
+uint8_t muse_offset = 70;
+uint16_t muse_tempo = 50;
+
+void matrix_scan_user(void) {
+  #ifdef AUDIO_ENABLE
+    if (muse_mode) {
+      if (muse_counter == 0) {
+        uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
+        if (muse_note != last_muse_note) {
+          stop_note(compute_freq_for_midi_note(last_muse_note));
+          play_note(compute_freq_for_midi_note(muse_note), 0xF);
+          last_muse_note = muse_note;
+        }
+      }
+      muse_counter = (muse_counter + 1) % muse_tempo;
+    }
+  #endif
+}
+
+bool music_mask_user(uint16_t keycode) {
+  switch (keycode) {
+    case LSPRSE:
+    case RSPLWR:
+      return false;
+    default:
+      return true;
+  }
+}
+
+int cur_dance (qk_tap_dance_state_t *state) {
+    if (state->count == 1) {
+    if (state->interrupted) {
+      if (!state->pressed) return SINGLE_TAP;
+      else return SINGLE_HOLD;
+    }
+    else {
+      if (!state->pressed) return SINGLE_TAP;
+      else return SINGLE_HOLD;
+    }
+  }
+  else if (state->count == 2) {
+    if (state->pressed) return DOUBLE_HOLD;
+    else return DOUBLE_TAP;
+  }
+  else if (state->count == 3) {
+    if (!state->pressed) return TRIPLE_TAP;
+    else return TRIPLE_HOLD;
+  }
+  else return 8;
+}
+
+static tap lstap_state = {
+  .is_press_action = true,
+  .state = 0
+};
+static tap rstap_state = {
+  .is_press_action = true,
+  .state = 0
+};
+
+void ls_finished (qk_tap_dance_state_t *state, void *user_data) {
+  lstap_state.state = cur_dance(state);
+  switch (lstap_state.state) {
+    case SINGLE_TAP: register_code16(KC_LPRN); break;
+    case SINGLE_HOLD: register_code(KC_LSHIFT); break;
+    case DOUBLE_TAP: register_code16(KC_LCBR); break;
+    case TRIPLE_TAP: 
+      SEND_STRING("<>| ' äöÄÖ lol");
+      /*PLAY_SONG(m4);*/ break; //register_code16(KC_LABK); break;
+    //case DOUBLE_HOLD: register_code(KC_LALT); break;
+    case DOUBLE_SINGLE_TAP: register_code16(KC_LPRN); unregister_code16(KC_LPRN); register_code16(KC_LPRN);
+  }
+}
+
+void ls_reset (qk_tap_dance_state_t *state, void *user_data) {
+  switch (lstap_state.state) {
+    case SINGLE_TAP: unregister_code16(KC_LPRN); break;
+    case SINGLE_HOLD: unregister_code(KC_LSHIFT); break;
+    case DOUBLE_TAP: unregister_code16(KC_LCBR); break;
+    case TRIPLE_TAP: break; // unregister_code16(KC_LABK); break;
+    //case DOUBLE_HOLD: unregister_code(KC_LALT);
+    case DOUBLE_SINGLE_TAP: unregister_code16(KC_LPRN);
+  }
+  lstap_state.state = 0;
+}
+
+void rs_finished (qk_tap_dance_state_t *state, void *user_data) {
+  rstap_state.state = cur_dance(state);
+  switch (rstap_state.state) {
+    case SINGLE_TAP: register_code16(KC_RPRN); break;
+    case SINGLE_HOLD: register_code(KC_RSHIFT); break;
+    case DOUBLE_TAP: register_code16(KC_RCBR); break;
+    case TRIPLE_TAP: register_code16(KC_RABK); break;
+    //case DOUBLE_HOLD: register_code(KC_LALT); break;
+    case DOUBLE_SINGLE_TAP: register_code16(KC_RPRN); unregister_code16(KC_RPRN); register_code16(KC_RPRN);
+  }
+}
+
+void rs_reset (qk_tap_dance_state_t *state, void *user_data) {
+  switch (rstap_state.state) {
+    case SINGLE_TAP: unregister_code16(KC_RPRN); break;
+    case SINGLE_HOLD: unregister_code(KC_RSHIFT); break;
+    case DOUBLE_TAP: unregister_code16(KC_RCBR); break;
+    case TRIPLE_TAP: unregister_code16(KC_RABK); break;
+    //case DOUBLE_HOLD: unregister_code(KC_LALT);
+    case DOUBLE_SINGLE_TAP: unregister_code16(KC_RPRN);
+  }
+  rstap_state.state = 0;
+}
+
+void bs_taphandler (qk_tap_dance_state_t *state, void *user_data) {
+  switch (state->count) {
+    case 1: register_code(KC_BSPC);unregister_code(KC_BSPC); break;
+    case 2: register_mods(MOD_BIT(KC_LCTL));register_code(KC_BSPC);unregister_code(KC_BSPC);unregister_mods(MOD_BIT(KC_LCTL)); break;
+  }
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [LSCD]     = ACTION_TAP_DANCE_FN_ADVANCED(NULL,ls_finished, ls_reset),
+  [RSCD]     = ACTION_TAP_DANCE_FN_ADVANCED(NULL,rs_finished, rs_reset),
+  [GRCL]   = ACTION_TAP_DANCE_DOUBLE(KC_GRV, KC_CALCULATOR),
+  [AUMENT]     = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_ENT),
+  [OUMAST]     = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_BSLS),
+  [DELPL]     = ACTION_TAP_DANCE_DOUBLE(KC_DEL, KC_MINS),
+
+  [BSPTD]      = /*ACTION_TAP_DANCE_DOUBLE(KC_BSPC, LCTL(KC_BSPC)),//*/ACTION_TAP_DANCE_FN_ADVANCED_TIME(bs_taphandler, NULL, NULL, 160), 
+};
