1fcc06499761b649838952e8e24fa3c62634eb83
Aapo Saaristo <aapo@unicornpirates.studio>
2019-04-05 08:36:19 +0300

    change KC_NO to XXXXXXX for readability

--- keyboards/preonic/keymaps/shinmai/keymap.c
+++ keyboards/preonic/keymaps/shinmai/keymap.c
@@ -223,7 +223,7 @@ TD(LSCD), TD(ZABRC),KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COM
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  \
   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_BSLS, KC_ENT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  \
-  KC_LCTL, KC_NO,   KC_LALT, KC_LGUI, GRSE,          KC_SPC,     KC_RALT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  KC_LCTL, XXXXXXX, KC_LALT, KC_LGUI, GRSE,          KC_SPC,     KC_RALT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 /* GuestRaise
