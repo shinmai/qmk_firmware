ce0dc6e0bbb1b5d499f68745d08e08ece355f3ea
Aapo Saaristo <aapo@unicornpirates.studio>
2019-04-06 19:20:30 +0300

    workaround UCIS not detecting LT() keycodes
    A separate UCIS layer where only alpha keys, esc, space and enter are available is enabled when UCLEAD is pressed, and disabled after the UCIS input is completed or cancelled.

--- keyboards/planck/keymaps/shinmai/keymap.c
+++ keyboards/planck/keymaps/shinmai/keymap.c
@@ -5,7 +5,7 @@
 extern keymap_config_t keymap_config;
 
 enum planck_layers {
-  _QWERTY,
+  _QWERTY = 0, _UCISB,
   _LOWER,
   _RAISE,
   _ADJUST,
@@ -152,8 +152,17 @@ void qk_ucis_start_user(void) {
   unicode_input_start();
   register_hex(0x25B7);
   unicode_input_finish();
+  layer_on(_UCISB);
 }
-void qk_ucis_symbol_fallback (void) {}
+void qk_ucis_success(uint8_t symbol_index) {
+  layer_off(_UCISB);
+}
+void qk_ucis_symbol_fallback (void) {
+  layer_off(_UCISB); 
+}
+void qk_ucis_cancel (void) {
+  layer_off(_UCISB);
+}
 #endif
 
 /*
@@ -204,6 +213,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, TD(OUMAST), KC_QUOT,
    TD(LSCD),TD(ZABRC),KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TD(RSCD),
     KC_LCTL,TD(HPRLD), KC_LALT, TD(GUIMN), LSPRSE,LSPRSE,RSPLWR,RSPLWR ,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+),
+
+/* UCIS BASE
+ * ,-----------------------------------------------------------------------------------.
+ * |      |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |      |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |    Space    |    Space    |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_UCISB] = LAYOUT_planck_grid(
+    XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    XXXXXXX, KC_ENT,
+    XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 /* Lower
