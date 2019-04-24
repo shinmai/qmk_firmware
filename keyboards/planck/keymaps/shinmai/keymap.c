bf89bdba9c52a8bb27b2aefae2cabe1db58a818c
Aapo Saaristo <aapo@unicornpirates.studio>
2019-04-24 11:46:54 +0300

    remove reduntant mappings

--- keyboards/planck/keymaps/shinmai/keymap.c
+++ keyboards/planck/keymaps/shinmai/keymap.c
@@ -341,7 +341,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    TD(GRCL), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,   KC_0,     KC_MINS,
     _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_RBRC, 
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, KC_RALT, _______,      _______,         _______,      KC_HOME, KC_PGDN, KC_PGUP, KC_END
+    _______, _______, KC_RALT, _______,      _______,         _______,      _______, _______, _______, _______
 ),
 
 /* Adjust
